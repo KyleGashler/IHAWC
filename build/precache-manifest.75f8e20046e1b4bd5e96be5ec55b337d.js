860f5ac81668e0fda6c7af5f28e766e0fcd88221
Kyle Gashler <kyle-gashler@pluralsight.com>
2020-09-19 10:29:16 -0600

    first commit project settup

---
+++ build/precache-manifest.75f8e20046e1b4bd5e96be5ec55b337d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02a1bb783ff8835c3ad8935969b98a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc14ef9323334959d604",
+    "url": "/static/js/2.4a3e95b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4a3e95b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499b4da1889b7f3b9584",
+    "url": "/static/js/main.1476625c.chunk.js"
+  },
+  {
+    "revision": "098c29596dff872817a0",
+    "url": "/static/js/runtime-main.70e74d09.js"
+  }
+]);
