07c3dd9e45fe67d5857e4f80a060f47a5d9ed147
Kyle Gashler <kyle-gashler@pluralsight.com>
2020-09-23 15:32:44 -0600

    temp swap

--- ihawc/src/index.js
+++ ihawc/src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-// import App from './App';
-import HomepageLayout from "./homepage";
+import App from './App';
+// import HomepageLayout from "./homepage";
 
-ReactDOM.render( <HomepageLayout /> , document.getElementById("root"))
+ReactDOM.render( <App /> , document.getElementById("root"))
