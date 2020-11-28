a8a9ff6834209751f629a8357da453c8a693c136
Kyle Gashler <kyle-gashler@pluralsight.com>
2020-11-28 13:38:46 -0700

    switch from react

--- ihawc/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-
-import App from './App';
-// import HomepageLayout from "./homepage";
-
-ReactDOM.render( <App /> , document.getElementById("root"))
